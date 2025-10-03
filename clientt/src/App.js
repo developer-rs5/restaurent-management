2905c3c863144e0ebeaf4977e8c1253e78a6711d
Developer RS | Rishabh Sharma <164728448+developer-rs5@users.noreply.github.com>
2025-10-03 16:11:25 +0530

    .

--- clientt/src/App.js
+++ clientt/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
-import {Route, Routes, BrowserRouter} from "rea"
+import {Route, Routes, BrowserRouter} from "react-router-dom"
 
 function App() {
   return (
